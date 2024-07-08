c1be01da8be01a9cdc38d1d2e1d608e8cb5f0fc4
Souvik Chakraborty <souvikchakraborty@Souviks-MacBook-Air.local>
2024-07-09 00:15:13 +0530

    Added Grpc Server

---
+++ src/main/java/greeting/server/GreetingServer.java
@@ -0,0 +1,32 @@
+package greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int port = 50051;
+        System.out.println("Starting Greeting Server");
+
+        Server server = ServerBuilder.forPort(port)
+                .build();
+
+        server.start();
+        System.out.println("Greeting Server Started");
+        System.out.println("Listening on port " + port);
+
+        Runtime.getRuntime()
+                .addShutdownHook(new Thread(
+                        () -> {
+                            System.out.println("received shutdown request");
+                            server.shutdown();
+                            System.out.println("Server Stopped");
+                        }
+                ));
+
+        //server will wait for termination
+        server.awaitTermination();
+    }
+}
